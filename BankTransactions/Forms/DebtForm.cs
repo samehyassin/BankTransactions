619ca065a21f96d300321d529383c8e8400fa0d6
samehyassin <40151494+samehyassin@users.noreply.github.com>
2019-05-28 18:21:47 +0200

    Buttons for Debt Form

--- BankTransactions/Forms/DebtForm.cs
+++ BankTransactions/Forms/DebtForm.cs
@@ -16,5 +16,17 @@ namespace BankTransactions.Forms
         {
             InitializeComponent();
         }
+
+       
+
+        private void frmDebtForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
